996fdf1dd56cdb50f66de132a8de0cf583697745
Saul Flores <saulpolo95@gmail.com>
2026-09-22 22:55:21 -0600

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums: number[]): number {
+        let map = new Map();
+
+        nums.forEach(num => {
+            if(map.has(num)) {
+                let val = Number(map.get(num)) + 1;
+                map.set(num, val);
+            } else {
+                map.set(num, 1)
+            }
+        })
+
+        const entries = map.entries();
+
+        for(let [key, value] of entries) {
+            if(value === 1) {
+                return key;
+            }
+        }
+    }
+}
